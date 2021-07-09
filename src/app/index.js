1b5c2c2b5b4c9a7f60973bda7ae20fb195e573e7
RoddoWasHere <roderich@renderheads.com>
2021-07-09 20:05:55 +0200

    initial commit

---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component{
+    render(){
+        return (<div>
+            <h1>Hello!</h1>
+        </div>);
+    }
+}
+
+var mountOn = document.getElementById("app"); 
+render(<App/>, mountOn);
