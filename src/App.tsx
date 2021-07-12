65b9f17d92cf0ff4500401a46a07aee761205c37
RoddoWasHere <roderich@renderheads.com>
2021-07-12 15:02:57 +0200

    Added TS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+import reactPng from "./React.png";
+import reactSvg from "./React-icon.svg";
+
+export const App = () => {
+    return (
+      <>
+        <h1>React TypeScript Webpack Starter Template</h1>
+        <img src={reactPng} />
+        <img src={reactSvg} />
+      </>
+    )
+  }
